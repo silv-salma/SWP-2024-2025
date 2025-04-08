386b2cef3e26b21a57a6985c1e3ec81a91adf0c4
silv-salma <KHA230102@spengergasse.at>
2025-04-08 14:58:53 +0200

    JSON

--- 2025-04-08_Blumenshop_JSON/script.js
+++ 2025-04-08_Blumenshop_JSON/script.js
@@ -1,3 +1,16 @@
+function loadJSON(file, callback) {
+  var xobj = new XMLHttpRequest();
+  xobj.overrideMimeType("application/json");
+  xobj.open("GET", file, true);
+  xobj.onreadystatechange = function () {
+    if (xobj.readyState == 4 && xobj.status == "200") {
+      // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+      callback(xobj.responseText);
+    }
+  };
+  xobj.send(null);
+}
+
 function zeigePreis() {
   const blumenAnzeigen = document.getElementById("blumenAnzeigen");
   const preisDiv = document.getElementById("preis");
@@ -48,3 +61,5 @@ function zeileEinfuegen() {
 
   zellePreis.innerHTML = `€${produktPreis}`;
 }
+
+function gesamtPreis() {}
